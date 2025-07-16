0861a507f73ec9689b5a5af67d81895225845b23
Rolan <gamegeograf@gmail.com>
2025-07-16 14:57:29 +0300

    3 try

--- src/ru/common/service/TaskManager.java
+++ src/ru/common/service/TaskManager.java
@@ -9,29 +9,41 @@ import java.util.List;
 public interface TaskManager {
 
     boolean addTask(Task task);
+
     boolean addEpic(Epic epic);
+
     boolean addSubtask(Subtask subtask);
 
     boolean containsIdEpic(int epicId); // Проверка существования эпика по ID
 
     List<Task> printTasks();
+
     List<Epic> printEpics();
+
     List<Subtask> printSubtasks();
 
     Task getTask(int id);
+
     Epic getEpic(int id);
+
     Subtask getSubtask(int id);
 
     boolean deleteTaskById(int id);
+
     boolean deleteEpicById(int id);
+
     boolean deleteSubtaskById(int id);
 
     void deleteAllTasks();
+
     void deleteAllSubtasks();
+
     void deleteAllEpics();
 
     boolean updateTask(Task newTask);
+
     boolean updateEpic(Epic newEpic);
+
     boolean updateSubtask(Subtask newSubtask);
 
     List<Integer> getSubtasksOfEpic(int epicId);
