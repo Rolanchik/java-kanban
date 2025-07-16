9d8538deea8d64c79a8fd7b7580c3c5a3397411e
Rolan <gamegeograf@gmail.com>
2025-07-16 15:00:08 +0300

    4 try

--- src/ru/common/service/InMemoryHistoryManager.java
+++ src/ru/common/service/InMemoryHistoryManager.java
@@ -6,7 +6,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public abstract class InMemoryHistoryManager implements HistoryManager {
+public class InMemoryHistoryManager implements HistoryManager {
 
     private final Map<Integer, Node> nodes = new HashMap<>();
     private Node head = null;
